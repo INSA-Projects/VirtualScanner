c597a8ad3d9d5122b487215495396dd15959b98e
SuperValou <valentin.esmieu@insa-rennes.fr>
2015-12-16 11:01:34 +0100

    Keyboard controller

---
+++ VirtualScanner/Assets/Scripts/Controller/KeyboardController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardController : MonoBehaviour
+{
+    // keyboard
+    public float keyboardSensitivity = 1;
+
+    // mouse
+    public float sensitivityX = 1F;
+    public float sensitivityY = 1F;
+
+    public float minimumX = -80F;
+    public float maximumX = 80F;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationY = 0F;
+
+
+    void Update()
+    {
+        // keyboard
+        
+        this.transform.position += this.transform.forward * Input.GetAxis("Vertical") * keyboardSensitivity;
+        this.transform.position += this.transform.right * Input.GetAxis("Horizontal") * keyboardSensitivity;
+        
+
+        // mouse
+        if (Input.GetMouseButton(0))
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+            transform.localEulerAngles = new Vector3(rotationY, rotationX, 0);
+        }
+    }
+
+
+}
